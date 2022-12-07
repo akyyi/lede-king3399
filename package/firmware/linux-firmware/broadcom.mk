54de19e6241df2103776ad82730b93a76c866387
unknown <yizhenqi@thewesthill.net>
2022-12-08 01:46:19 +0800

    Revert "dtb rm p2"
    This reverts commit 9044c07c27294bbca7d2efbceb6df1ebc2c551a5.

--- package/firmware/linux-firmware/broadcom.mk
+++ package/firmware/linux-firmware/broadcom.mk
@@ -117,6 +117,16 @@ define Package/bnx2x-firmware/install
 		$(PKG_BUILD_DIR)/bnx2x/* \
 		$(1)/lib/firmware/bnx2x/
 endef
+$(eval $(call BuildPackage,bnx2x-firmware))
+
+Package/station-p2-firmware = $(call Package/firmware-default,Broadcom FullMac SDIO firmware)
+define Package/station-p2-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/brcm
+	$(INSTALL_DATA) ./brcm_firmware/ap6275s/BCM4362A2.hcd $(1)/lib/firmware/brcm/BCM4362A2.hcd
+	$(INSTALL_DATA) ./brcm_firmware/ap6275s/clm_bcm43752a2_ag.blob $(1)/lib/firmware/brcm/brcmfmac43752-sdio.clm_blob
+	$(INSTALL_DATA) ./brcm_firmware/ap6275s/fw_bcm43752a2_ag_apsta.bin $(1)/lib/firmware/brcm/brcmfmac43752-sdio.firefly,rk3568-roc-pc.bin
+	$(INSTALL_DATA) ./brcm_firmware/ap6275s/nvram_ap6275s.txt $(1)/lib/firmware/brcm/brcmfmac43752-sdio.firefly,rk3568-roc-pc.txt
+endef
 $(eval $(call BuildPackage,station-p2-firmware))
 
 Package/rp-king3399-firmware = $(call Package/firmware-default,Broadcom FullMac SDIO firmware)
